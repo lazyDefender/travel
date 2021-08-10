9581580ec8129b14631f814cfcb54cbb925d1e97
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-08-10 19:17:46 +0300

    Add swagger

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,16 @@
-require('dotenv').config();
+import dotenv from 'dotenv'; 
 
 import express from 'express';
 import cors from 'cors';
+
+import swaggerUI from 'swagger-ui-express';
+import { swaggerSpec } from './config/swagger';
+
 import { initializeFirebase} from './firebase';
 import { responseMiddleware } from './middlewares/response.middleware';
 import routes from './routes';
+
+dotenv.config();
 
 initializeFirebase();
 
@@ -13,6 +19,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 routes(app);
 app.use(responseMiddleware);
@@ -21,5 +28,3 @@ const port = 3050;
 app.listen(process.env.PORT || port, () => {
     console.log(`started on port ${port}`);
 });
-
-exports.app = app;
