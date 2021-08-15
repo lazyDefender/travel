bf0021547f12f08986477e4cb092b86dd0988927
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-08-15 14:21:44 +0300

    Refactor sign in  page

--- client/src/bus/root/components/LoginForm.jsx
+++ client/src/bus/root/components/LoginForm.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Redirect, useHistory } from 'react-router-dom'
+import React, { useEffect, useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
 import {
   Formik, 
   Form, 
@@ -15,7 +15,6 @@ import {
 import MomentUtils from '@date-io/moment'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import Box from '@material-ui/core/Box'
-import { useSelector } from 'react-redux'
 
 import { authActions } from '../../../redux/auth.slice'
 import store from '../../../redux/store'
@@ -28,14 +27,6 @@ import useFirstLoadedPage from '../../../global/hooks/useFirstLoadedPage'
 import GoHomeBar from '../../../global/components/GoHomeBar'
 import { snackbarActions } from '../../../redux/snackbar.slice'
 import useSnackbar from '../../../global/hooks/useSnackbar'
-
-const signInWithGoogle = () => {
-  store.dispatch(authActions.signInWithProvider('google'))
-}
-
-const signInWithFacebook = () => {
-  store.dispatch(authActions.signInWithProvider('facebook'))
-}
 
 const SignUpForm = () => {
   const history = useHistory();
@@ -58,84 +49,97 @@ const SignUpForm = () => {
     }
   }, [error]);
 
-  const formJSX = <div>
-    <Typography>Вхід</Typography>
-    <Formik
+  const signInWithGoogle = useCallback(() => {
+    store.dispatch(authActions.signInWithProvider('google'));
+  }, []);
+  
+  const signInWithFacebook = useCallback(() => {
+    store.dispatch(authActions.signInWithProvider('facebook'));
+  }, []);
+
+  const onSignup = useCallback(() => {
+    history.replace('/signup');
+  }, []);
+
+  const onSubmit = (values, { setSubmitting }) => {
+    store.dispatch(authActions.signIn(values));
+  };
+
+  const formJSX = (
+    <div>
+      <Typography>Вхід</Typography>
+      <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={async (values, { setSubmitting }) => {
-            store.dispatch(authActions.signIn(values));
-        }}
-    >
-    {({submitForm, isSubmitting, touched, errors}) => (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-        <Form>
-            <Box margin={1}>
+        onSubmit={onSubmit}
+      >
+        {({ submitForm, isSubmitting, touched, errors }) => (
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <Form>
+              <Box margin={1}>
                 <Field
-                component={TextField}
-                name="email"
-                label="Email"
-                disabled={false}
+                  component={TextField}
+                  name="email"
+                  label="Email"
+                  disabled={false}
                 />
-            </Box>
-            <Box margin={1}>
+              </Box>
+              <Box margin={1}>
                 <Field
-                component={TextField}
-                type="password"
-                name="password"
-                label="Пароль"
-                disabled={false}
+                  component={TextField}
+                  type="password"
+                  name="password"
+                  label="Пароль"
+                  disabled={false}
                 />
-            </Box>
-           <Box margin={1}>
+              </Box>
+              <Box margin={1}>
                 <Button
-                variant="contained"
-                color="primary"
-                onClick={submitForm}
+                  variant="contained"
+                  color="primary"
+                  onClick={submitForm}
                 >
-                Увійти
+                  Увійти
                 </Button>
                 <Button
-                variant="contained"
-                color="default"
-                onClick={() => {
-                  history.replace('/signup')
-                }}
+                  variant="contained"
+                  color="default"
+                  onClick={onSignup}
                 >
-                Зареєструватись
+                  Зареєструватись
                 </Button>
-            </Box>
-            <Box margin={1}>
+              </Box>
+              <Box margin={1}>
                 <Button
-                variant="contained"
-                color="primary"
-                onClick={signInWithGoogle}
+                  variant="contained"
+                  color="primary"
+                  onClick={signInWithGoogle}
                 >
-                Увійти через Google
+                  Увійти через Google
                 </Button>
-            </Box>
-            <Box margin={1}>
+              </Box>
+              <Box margin={1}>
                 <Button
-                variant="contained"
-                color="primary"
-                onClick={signInWithFacebook}
+                  variant="contained"
+                  color="primary"
+                  onClick={signInWithFacebook}
                 >
-                Увійти через Facebook
-                </Button>
-                
-            </Box>
-        </Form>
-    </MuiPickersUtilsProvider>
-    )}
-    </Formik>
-</div>
+                  Увійти через Facebook
+                </Button>                
+              </Box>
+            </Form>
+          </MuiPickersUtilsProvider>
+        )}
+      </Formik>
+    </div>
+  );
 
   const page = <>
     <GoHomeBar/>
     {formJSX}
-  </>
+  </>;
 
   return page;
 }
 
-export default SignUpForm
+export default SignUpForm;
