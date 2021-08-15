160142338c9deb437d670d3b57c639086678e3ec
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-08-15 14:57:02 +0300

    Remove default reducer

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,21 +1,18 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-import { toursFilter } from './toursFilter.slice'
-import { auth } from './auth.slice'
-import { hotel } from './hotel.slice'
-import { reservation } from './reservation.slice'
-import { orders } from './orders.slice'
+import { toursFilter } from './toursFilter.slice';
+import { auth } from './auth.slice';
+import { hotel } from './hotel.slice';
+import { reservation } from './reservation.slice';
+import { orders } from './orders.slice';
 import { snackbar } from './snackbar.slice';
-import { defaultReducer } from './default/reducer'
 
 export const rootReducer = combineReducers({
     // reducers
     toursFilter,
-    // tours,
     auth,
     hotel,
     reservation,
     orders,
     snackbar,
-    // defaultReducer,
 });
