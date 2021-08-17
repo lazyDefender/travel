fca78bcedd0fa21371b53f420f2ef9649a3dc956
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-08-17 13:36:02 +0300

    Refactor hotel component

--- client/src/bus/hotels/index.jsx
+++ client/src/bus/hotels/index.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import HotelsToursList from './components/HotelToursList'
-import HotelMain from './components/HotelMain'
-import useHotel from './hooks/useHotel'
-import useToursByHotel from './hooks/useToursByHotel'
-import GoogleMap from './components/GoogleMap'
-import AuthBar from '../../global/components/AuthBar'
-import Progress from '../../global/components/Progress'
+import HotelsToursList from './components/HotelToursList';
+import HotelMain from './components/HotelMain';
+import useHotel from './hooks/useHotel';
+import useToursByHotel from './hooks/useToursByHotel';
+import AuthBar from '../../global/components/AuthBar';
+import Progress from '../../global/components/Progress';
 
 
 const Hotel = () => {
     let { id } = useParams();
-    const { data: hotel, isFetching: isFetchingHotel, error: hotelError } = useHotel(id)
-    const { data: tours } = useToursByHotel(id)
+    const { 
+        data: hotel,
+        isFetching: isFetchingHotel, 
+        error: hotelError 
+    } = useHotel(id);
+    const { data: tours } = useToursByHotel(id);
     const hotelJSX = hotelError ? <h1>Готель не знайдено</h1> : <>
         <HotelMain hotel={hotel} />
         <HotelsToursList tours={tours} />
-    </>
+    </>;
     return (
         <>
             <AuthBar />
             {isFetchingHotel ? <Progress /> : hotelJSX}
         </>
-    )
-}
+    );
+};
 
-export default Hotel
+export default Hotel;
