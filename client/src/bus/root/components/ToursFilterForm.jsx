7f5c6c7adf07278f6ca7f8a5828e75885b58ae87
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-08-18 14:25:26 +0300

    Refactor root page

--- client/src/bus/root/components/ToursFilterForm.jsx
+++ client/src/bus/root/components/ToursFilterForm.jsx
@@ -15,11 +15,8 @@ import {
 } from 'formik-material-ui-pickers';
 import MomentUtils from '@date-io/moment';
 import moment from 'moment';
-import firebase from 'firebase';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 
-import { toursFilterActions } from '../../../redux/toursFilter.slice';
-import useToursFilterFormState from '../hooks/useToursFilterFormState';
 import '../../../moment-locales/uk';
 
 moment.locale('uk');
@@ -32,10 +29,11 @@ initialDate.set({
     millisecond: 0,
 });
 
-const ToursFilterForm = ({ cities }) => {
-  const dispatch = useDispatch();
-  const formState = useToursFilterFormState();
-
+const ToursFilterForm = ({ 
+  cities,
+  formState,
+  onSubmit,
+}) => {
   const initialValues = {
     toCity: formState?.toCity || cities[0]?.id,
     datetime: formState?.datetime || initialDate,
@@ -50,27 +48,6 @@ const ToursFilterForm = ({ cities }) => {
     if(!values.toCity) errors.toCity = 'Виберіть місто';
     return errors;
   };
-  
-  const onSubmit = (values, { setSubmitting }) => {
-    setSubmitting(true);
-
-    const datetime = firebase
-      .firestore
-      .Timestamp
-      .fromDate(values.datetime.toDate())
-      .toMillis();
-
-    dispatch(toursFilterActions.getTours({
-      ...values,
-      datetime, 
-    }));
-    dispatch(toursFilterActions.setFormState({
-      ...values,
-      datetime,
-    }));
-
-    setSubmitting(false);
-  };
 
   return (
     <Box>
@@ -79,117 +56,120 @@ const ToursFilterForm = ({ cities }) => {
         validate={validate}
         onSubmit={onSubmit}
       >
-        {({submitForm, isSubmitting, touched, errors}) => (
-          <MuiPickersUtilsProvider locale="uk" utils={MomentUtils}>
-            <Form>
-              <Grid 
-                container 
-                direction="row" 
-                alignItems="center"
-                justify="space-between"
-              >
-                <Grid item>
-                  <Box margin={1}>
-                    <Field
-                      component={TextField}
-                      type="text"
-                      name="toCity"
-                      label="Куди"
-                      select
-                      variant="outlined"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                    >
-                      {cities?.map((city) => (
-                        <MenuItem key={city.id} value={city.id}>
-                          {city.name}
+        {({submitForm, isSubmitting, touched, errors}) => {
+          return (
+            <MuiPickersUtilsProvider locale="uk" utils={MomentUtils}>
+              <Form>
+                
+                <Grid 
+                  container 
+                  direction="row" 
+                  alignItems="center"
+                  justify="space-between"
+                >
+                  <Grid item>
+                    <Box margin={1}>
+                      <Field
+                        component={TextField}
+                        type="text"
+                        name="toCity"
+                        label="Куди"
+                        select
+                        variant="outlined"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                      >
+                        {cities?.map((city) => (
+                          <MenuItem key={city.id} value={city.id}>
+                            {city.name}
+                          </MenuItem>
+                        ))}
+                      </Field>
+                    </Box>
+                  </Grid>
+                  
+                  <Grid item>
+                    <Box margin={1}>
+                      <Field 
+                        component={DatePicker} 
+                        inputVariant="outlined"
+                        disablePast={true} 
+                        name="datetime" 
+                        label="Початок туру"
+                        cancelLabel="Скасувати" 
+                      />
+                    </Box>
+                  </Grid>
+                  
+                  <Grid item>
+                    <Box margin={1}>
+                      <Field
+                        component={TextField}
+                        type="text"
+                        name="duration"
+                        label="Ночей"
+                        select
+                        variant="outlined"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                      >
+                        <MenuItem value={8}>
+                          8
                         </MenuItem>
-                      ))}
-                    </Field>
-                  </Box>
-                </Grid>
-                
-                <Grid item>
-                  <Box margin={1}>
-                    <Field 
-                      component={DatePicker} 
-                      inputVariant="outlined"
-                      disablePast={true} 
-                      name="datetime" 
-                      label="Початок туру"
-                      cancelLabel="Скасувати" 
-                    />
-                  </Box>
-                </Grid>
-                
-                <Grid item>
-                  <Box margin={1}>
-                    <Field
+                      </Field>
+                    </Box>
+                  </Grid>
+  
+                  <Grid item>
+                    <Box margin={1}>
+                      <Field
                       component={TextField}
-                      type="text"
-                      name="duration"
-                      label="Ночей"
-                      select
                       variant="outlined"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
+                        name="adultsCount"
+                        type="number"
+                        label="К-сть дорослих"
+                        inputProps={{
+                          min: 1,
+                          max: 6,
+                        }}
+                      />
+                    </Box>
+                  </Grid>
+  
+                  <Grid item>
+                    <Box margin={1}>
+                      <Field
+                        component={TextField}
+                        variant="outlined"
+                        name="kidsCount"
+                        type="number"
+                        label="К-сть дітей"
+                        inputProps={{
+                          min: 0,
+                          max: 6,
+                        }}
+                      />
+                    </Box>
+                  </Grid>
+                  
+                  <Box margin={1}>
+                    <Button
+                      variant="contained"
+                      disableElevation
+                      color="primary"
+                      disabled={isSubmitting}
+                      onClick={submitForm}
                     >
-                      <MenuItem value={8}>
-                        8
-                      </MenuItem>
-                    </Field>
+                      Знайти
+                    </Button>
                   </Box>
                 </Grid>
-
-                <Grid item>
-                  <Box margin={1}>
-                    <Field
-                    component={TextField}
-                    variant="outlined"
-                      name="adultsCount"
-                      type="number"
-                      label="К-сть дорослих"
-                      inputProps={{
-                        min: 1,
-                        max: 6,
-                      }}
-                    />
-                  </Box>
-                </Grid>
-
-                <Grid item>
-                  <Box margin={1}>
-                    <Field
-                      component={TextField}
-                      variant="outlined"
-                      name="kidsCount"
-                      type="number"
-                      label="К-сть дітей"
-                      inputProps={{
-                        min: 0,
-                        max: 6,
-                      }}
-                    />
-                  </Box>
-                </Grid>
-                
-                <Box margin={1}>
-                  <Button
-                    variant="contained"
-                    disableElevation
-                    color="primary"
-                    disabled={isSubmitting}
-                    onClick={submitForm}
-                  >
-                    Знайти
-                  </Button>
-                </Box>
-              </Grid>
-            </Form>
-          </MuiPickersUtilsProvider>
-        )}
+              </Form>
+            </MuiPickersUtilsProvider>
+          )
+        }}
       </Formik>
     </Box>
   );
