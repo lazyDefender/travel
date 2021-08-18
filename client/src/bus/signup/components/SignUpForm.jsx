c729dd0ea1ed6407278076e1792e44df16b9303b
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-08-18 17:44:54 +0300

    Improve sign up form ui

--- client/src/bus/signup/components/SignUpForm.jsx
+++ client/src/bus/signup/components/SignUpForm.jsx
@@ -10,6 +10,7 @@ import {
   Box,
   Typography,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import {
   TextField,
 } from 'formik-material-ui';
@@ -26,14 +27,33 @@ const initialValues = {
   password: '',
 };
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  form: {
+    width: '300px',
+  },
+  formElement: {
+    width: '100%',
+  }
+
+}));
+
 const SignUpForm = ({ 
   onSignInWithGoogle,
   onSignInWithFacebook,
   onLogin,
   onSubmit,
 }) => {
+  const classes = useStyles();
+
   const formJSX = (
-    <div>
+    <div className={classes.root}>
       <Typography>Зареєструватись</Typography>
       <Formik
         initialValues={initialValues}
@@ -42,72 +62,83 @@ const SignUpForm = ({
       >
         {({submitForm, isSubmitting, touched, errors}) => (
           <MuiPickersUtilsProvider utils={MomentUtils}>
-              <Form>
+              <Form className={classes.form}>
                 <Box margin={1}>
-                  <Field
+                  <Field className={classes.formElement}
                     component={TextField}
                     type="text"
                     name="firstName"
                     label="Ім'я"
                     disabled={false}
+                    variant="outlined"
                   />
                 </Box>
                 <Box margin={1}>
-                  <Field
+                  <Field className={classes.formElement}
                     component={TextField}
                     type="text"
                     name="lastName"
                     label="Прізвище"
                     disabled={false}
+                    variant="outlined"
                   />
                 </Box>
                 <Box margin={1}>
-                  <Field
+                  <Field className={classes.formElement}
                     component={TextField}
                     name="email"
                     label="Email"
                     disabled={false}
+                    variant="outlined"
                   />
                 </Box>
                 <Box margin={1}>
-                  <Field
+                  <Field className={classes.formElement}
                     component={TextField}
                     type="password"
                     name="password"
                     label="Пароль"
                     disabled={false}
+                    variant="outlined"
                   />
                 </Box>
                 <Box margin={1}>
-                  <Button
+                  <Button className={classes.formElement}
                     variant="contained"
                     color="primary"
                     onClick={submitForm}
+                    disableElevation
                   >
                     Готово
                   </Button>
-                  <Button
+                  
+                </Box>
+                <Box margin={1}>
+                  <Button className={classes.formElement}
                     variant="contained"
                     color="default"
                     onClick={onLogin}
+                    disableElevation
                   >
                     Увійти
                   </Button>
                 </Box>
                 <Box margin={1}>
-                  <Button
+                  <Button className={classes.formElement}
                     variant="contained"
                     color="primary"
                     onClick={onSignInWithGoogle}
+                    disableElevation
                   >
                     Увійти через Google
                   </Button>
                 </Box>
                 <Box margin={1}>
-                  <Button
+                  <Button className={classes.formElement}
                     variant="contained"
                     color="primary"
                     onClick={onSignInWithFacebook}
+                    disableElevation
                   >
                     Увійти через Facebook
                   </Button>
