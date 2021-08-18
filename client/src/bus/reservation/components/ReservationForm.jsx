ff2959f1b42c4bc81965fde2b88206cd00d58ee2
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-08-18 18:37:46 +0300

    Improve reservation form ui

--- client/src/bus/reservation/components/ReservationForm.jsx
+++ client/src/bus/reservation/components/ReservationForm.jsx
@@ -8,6 +8,7 @@ import {
   Button,
   Box
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import {
   TextField,
 } from 'formik-material-ui';
@@ -20,6 +21,10 @@ import moment from 'moment';
 
 import Progress from '../../../global/components/Progress'
 
+import '../../../moment-locales/uk';
+
+moment.locale('uk');
+
 const initialDate = moment();
 initialDate.set({
     hour: 0,
@@ -28,7 +33,25 @@ initialDate.set({
     millisecond: 0,
 });
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  form: {
+    width: '300px',
+  },
+  formElement: {
+    width: '100%',
+  }
+}));
+
 const ReservationForm = ({ tour, onSubmit, isFetching }) => {
+  const classes = useStyles();
+
   const initialValues = {
     toCity: '',
     datetime: initialDate,
@@ -42,16 +65,18 @@ const ReservationForm = ({ tour, onSubmit, isFetching }) => {
     return errors
   };
 
-  const formJsx = <Formik
+  const formJsx = <div className={classes.root}>
+  <Formik
     initialValues={initialValues}
     validate={validate}
     onSubmit={onSubmit}
   >
     {({submitForm, isSubmitting, touched, errors}) => (
       <MuiPickersUtilsProvider utils={MomentUtils}>
-        <Form>
+        <Form className={classes.form}>
           <Box margin={1}>
             <Field
+              className={classes.formElement}
               component={TextField}
               type="text"
               name="toCity"
@@ -63,11 +88,13 @@ const ReservationForm = ({ tour, onSubmit, isFetching }) => {
                 shrink: true,
               }}
               disabled
+              variant="outlined"
             >  
             </Field>
           </Box>
           <Box margin={1}>
             <Field
+              className={classes.formElement}
               component={TextField}
               type="text"
               name="hotel"
@@ -79,19 +106,23 @@ const ReservationForm = ({ tour, onSubmit, isFetching }) => {
                 shrink: true,
               }}
               disabled
+              variant="outlined"
             >  
             </Field>
           </Box>
           <Box margin={1}>
               <Field 
+                className={classes.formElement}
                 component={DatePicker} 
                 disablePast
                 name="datetime" 
                 label="Початок туру"
+                inputVariant="outlined"
               />
           </Box>
           <Box margin={1}>
             <Field
+              className={classes.formElement}
               component={TextField}
               type="text"
               name="duration"
@@ -103,38 +134,45 @@ const ReservationForm = ({ tour, onSubmit, isFetching }) => {
                 shrink: true,
               }}
               disabled
+              variant="outlined"
             >
             </Field>
           </Box>
           <Box margin={1}>
             <Field
+              className={classes.formElement}
               component={TextField}
               name="adultsCount"
               type="number"
-              label="Adults"
+              label="К-сть дорослих"
               inputProps={{
                   min: 1,
                   max: tour?.hotel?.maxAdultsCount || 6
               }}
+              variant="outlined"
             />
           </Box>
           <Box margin={1}>
             <Field
+              className={classes.formElement}
               component={TextField}
               name="kidsCount"
               type="number"
-              label="Kids"
+              label="К-сть дітей"
               inputProps={{
                   min: 0,
                   max: tour?.hotel?.maxKidsCount || 6
               }}
+              variant="outlined"
             />
           </Box>
           <Box margin={1}>
             <Button
+              className={classes.formElement}
               variant="contained"
               color="primary"
               onClick={submitForm}
+              disableElevation
             >
               Забронювати
             </Button>
@@ -143,6 +181,7 @@ const ReservationForm = ({ tour, onSubmit, isFetching }) => {
       </MuiPickersUtilsProvider>
     )}
   </Formik>
+  </div>;
 
     const jsx = isFetching ? <Progress /> : formJsx;
 
